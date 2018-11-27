5d2a9de9f47be23abd318c7473b05dc159258b5c
mstarac <43557753+mstarac@users.noreply.github.com>
2018-11-27 12:47:39 -0800

    Boolean Car Insurance Drill

---
+++ BooleanCarInsurance/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+
+namespace BooleanCarInsurance
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("What is your age?");
+            byte ageYour = Convert.ToByte(Console.ReadLine());
+
+            Console.WriteLine("Have you ever had a DUI? Answer true or false");
+            bool hadADui = Convert.ToBoolean(Console.ReadLine());
+
+            Console.WriteLine("How many speeding tickets do you have?");
+            byte speedingTickets = Convert.ToByte(Console.ReadLine());
+
+            Console.WriteLine("Does the applicant qualify for car insurance?");
+            Console.WriteLine(ageYour > 15 && !hadADui && speedingTickets < 4);
+            Console.ReadLine();
+
+        }
+        
+    }
+}
