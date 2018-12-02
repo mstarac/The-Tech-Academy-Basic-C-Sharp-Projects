728c030ba40229ae601163c5f959156b1af40745
mstarac <43557753+mstarac@users.noreply.github.com>
2018-12-01 16:33:23 -0800

    MethodsOne

---
+++ MethodsOne/MathOperationD1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsOne
+{
+    class MathOperationD1
+    {
+        public class Math1
+        {
+            public Math1(int a)
+            {
+                this.a = a + 2;
+            }
+            public int a { get; set; }
+        }
+        public class Math2
+        {
+            public Math2(int b)
+            {
+                this.b = b * 2;
+            }
+            public int b { get; set; }
+        }
+        public class Math3
+        {
+            public Math3(int c)
+            {
+                this.c = c / 2;
+            }
+            public int c { get; set; }
+        }
+    }
+
+
+
