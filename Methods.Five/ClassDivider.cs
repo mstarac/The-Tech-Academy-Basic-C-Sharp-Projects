e4451c8415ac3a6da4ba9bed142151ab5cde7628
mstarac <43557753+mstarac@users.noreply.github.com>
2018-12-03 13:08:12 -0800

    Methods Five

---
+++ Methods.Five/ClassDivider.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods.Five
+{
+    class ClassDivider
+    {
+        public void Division(int M, out int N)
+        {
+            N = M / 2;
+        }
+        public static void Division(decimal P, out decimal Q)
+        {
+            Q = P / 2;
+        }
+        int M { get; set; }
+        int N { get; set; }
+        decimal P { get; set; }
+        decimal Q { get; set; }
+    }
+}
